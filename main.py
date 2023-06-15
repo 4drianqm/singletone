d234e3a4c184c49c80853ab6badc6e22746279f6
Facutad de Ingenieria <ingenieria@LaboratoriosCQ.com.gt>
2023-06-15 11:40:07 -0600

    instancias diferentes

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from database import Database
 
 def modulo1():
     db1 = Database()
-    db1.create_user('Adrian')
+    # db1.create_user('Adrian')
+    return db1
 
 
 def modulo2():
     db2 = Database()
-    db2.create_user('Fernanda')
+    # db2.create_user('Fernanda')
+    return db2
 
 
-modulo1()
-modulo2()
+db_a = modulo1()
+db_b = modulo2()
+
+if id(db_a) == id(db_b):
+    print('the instances are the same')
+else:
+    print('the instances are different')
